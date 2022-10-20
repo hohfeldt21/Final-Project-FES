90ddbe19b96e4f989b0a3ae6c5e5bbf224c28c1f
hohfeldt21 <hohfeldt@gmail.com>
2022-10-20 15:33:03 -0400

    empty commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./Footer";
 import MoviePage from "./MoviePage";
